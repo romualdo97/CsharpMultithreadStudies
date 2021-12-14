2d6f403573a373802586e92d96aeac71874a64fd
Romualdo Villalobos <romualdo.villalobos@wizard.com.co>
2021-12-13 19:06:40 -0500

    Very simple example of spinning

---
+++ Spinning/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Spinning
+{
+    class Program
+    {
+        public static bool done = false;
+
+        static void Main(string[] args)
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    Console.WriteLine("Task started");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Task ended");
+                }
+                finally
+                {
+                    done = true;
+                }
+            });
+
+            SpinWait.SpinUntil(() => done);
+            // StupidSpinning();
+
+            Console.WriteLine("Ended");
+        }
+
+        private static void StupidSpinning ()
+        {
+            while (!done)
+            {
+                Thread.Sleep(10);
+                // Wait by polling
+            }
+        }
+    }
+}
